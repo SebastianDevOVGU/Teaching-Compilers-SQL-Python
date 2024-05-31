3f8c1f73e84f4de48abd1fdec1f8c048fd3f43c6
SebastianDevOVGU <sebastian4.schaefer@st.ovgu.de>
2024-05-31 14:09:19 +0200

    Added comments for better understanding

--- coding/query.py
+++ coding/query.py
@@ -3,18 +3,20 @@ import torch
 class QueryModule(torch.nn.Module):
     def __init__(self):
         super(QueryModule, self).__init__()
-
+    #returns the table specified by its name
     def from_table(self, tables, tableDict, tableSelected):
         return tables[tableDict[tableSelected]]
-
+    #returns a table with only the specified rows (by index)
     def select_rows(self, table, rows):
         indices = torch.tensor(rows)
         return torch.index_select(table, 0, indices)
-
+    #returns a table with only the specified columns (by index)
     def select_column(self, table, columns):
         indices = torch.tensor(columns)
         return torch.index_select(table, 1, indices)
 
+
+#Tests
 qm = QueryModule()
 
 tableDict = dict([("A",0), ("B",1),("C",2)])
