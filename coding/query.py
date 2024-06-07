312a441978a927b3190364487aadd1b2670821f2
SebastianDevOVGU <sebastian4.schaefer@st.ovgu.de>
2024-06-07 13:12:02 +0200

    Added a function to perform a filter operation regarding a condition statement

--- coding/query.py
+++ coding/query.py
@@ -4,8 +4,8 @@ class QueryModule(torch.nn.Module):
     def __init__(self):
         super(QueryModule, self).__init__()
     #returns the table specified by its name
-    def from_table(self, tables, tableDict, tableSelected):
-        return tables[tableDict[tableSelected]]
+    def from_table(self, tableDict, tableSelected):
+        return tableDict[tableSelected]
     #returns a table with only the specified rows (by index)
     def select_rows(self, table, rows):
         indices = torch.tensor(rows)
@@ -14,15 +14,34 @@ class QueryModule(torch.nn.Module):
     def select_column(self, table, columns):
         indices = torch.tensor(columns)
         return torch.index_select(table, 1, indices)
+    def filter_rows(self, table, filter_cond, filter_value, filter_column):
+        if filter_cond == "greater":
+            mask = self.select_column(table, filter_column) > filter_value
+        elif filter_cond == "equal":
+            mask = self.select_column(table, filter_column) == filter_value
+        elif filter_cond == "less":
+            mask = self.select_column(table, filter_column) < filter_value
+        indices = torch.nonzero(mask)
+        return table[indices]
 
 
 #Tests
 qm = QueryModule()
 
-tableDict = dict([("A",0), ("B",1),("C",2)])
+iree_qm = torch.compile(qm, backend="turbine_cpu")
 tables = []
 for _ in range (0,3):
     tables.append(torch.rand((10,10), requires_grad=False, dtype=torch.float))
-print(qm.from_table(tables, tableDict, "B"))
-print(qm.select_rows(qm.from_table(tables, tableDict, "B"), [2,7]))
-print(qm.select_column(qm.from_table(tables, tableDict, "B"),[0,1]))
+tableDict = dict([("A",tables[0]), ("B",tables[1]),("C",tables[2])])
+print("Full table B:")
+print(qm.from_table(tableDict, "B"))
+print("Table B with rows 2 and 7:")
+print(qm.select_rows(qm.from_table(tableDict, "B"), [2,7]))
+print("Table B with columns 0 and 1:")
+print(qm.select_column(qm.from_table(tableDict, "B"),[0,1]))
+print("Table B with rows that have a value greater than 0.5 in the first column:")
+print(qm.filter_rows(qm.from_table(tableDict, "B"), "greater", 0.5, [0]))
+print("Table B with rows that have a value equal to 0.5 in the third column:")
+print(qm.filter_rows(qm.from_table(tableDict, "B"), "equal", 0.5, [2]))
+print("Table B with rows that have a value less than 0.5 in the second column:")
+print(qm.filter_rows(qm.from_table(tableDict, "B"), "less", 0.5, [1]))
